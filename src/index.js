c176271d29f3024faac4b5312118dc64073ab374
WWWolkodaWWW <80260640+WWWolkodaWWW@users.noreply.github.com>
2022-02-17 02:50:19 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
+  const newArr = [];
 
-  if (str = [['(', ')']]) { 
-    return true;
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    newArr.push(bracketsConfig[i].join(''));
   }
-  
 
+  for (let j = 0; j < newArr.length; j++) { 
+    if (str.includes(newArr[j])) { 
+      str = str.replace(newArr[j], '');
+      j = -1;
+    }
+  }
+
+  if (str.length === 0) {
+    return true
+  } else { 
+    return false;
+  }
 
 }
